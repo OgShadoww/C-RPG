f43181c67d885d52c833630e50131de8c16a3fae
Orest <galenzao@gmail.com>
2025-02-20 16:35:11 +0200

    fix

--- src/player.c
+++ src/player.c
@@ -1,8 +1,35 @@
 #include<stdio.h>
 
-struct Player {
-    char name[20];
+typedef struct {
+    char name[50];
+    int level;
+    int xp;
+    int xp_needed;
+    int strength;
+    int intelligence;
+    int stamina;
+    char last_login[20];
+} Player;
 
-};
+int loadPlayer() {
+    FILE *player = fopen("data/player.txt", "r");
 
-FILE *File = fopen("p")
+    if (!player) {
+        printf("Error: Cannot open file\n");
+        return 0;
+    }
+
+    char f[100];
+    while(fgets(f, sizeof(f), player)) {
+        printf("%s", f);
+    }
+
+    fclose(player);
+    return 1;
+}
+
+int main() {
+    loadPlayer();
+    
+    return 0;
+}
